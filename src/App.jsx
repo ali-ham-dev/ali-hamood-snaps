523c8378e5fdaf3158033f820546ef3cf9937f6f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-12 10:18:34 -0800

    Started building out the header and made various updates to make the header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <Header />
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+          {/* <Route path='*' element={<Home />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import FilterSvg from '../../assets/icons/Filter.svg';
+import './Header.css';
+
 function Header() {
     return (
-        <header>
-        <h1>My Blog</h1>
+        <header className="header">
+            <h1 className="site-logo">
+                <NavLink className="site-logo__link" to="/">Snaps</NavLink>
+            </h1>
+            <button className="filter">
+                <label className="filter__text">Filter</label>
+                <div className="filter__icon-container">
+                    <img className="filter__icon" src={ FilterSvg } alt="Filter icon."></img>
+                </div>
+            </button>
         </header>
     );
 }
