fb76565b4a7a98f913df3f6e276bec9fe62edbb3
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-17 13:23:40 -0800

    Squash merge snaps-v01.00.0001.01-feature. Adds header and some of hero as well as setting up global styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Header />
+        <Hero />
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+          {/* <Route path='*' element={<Home />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import FilterSvg from '../../assets/icons/Filter.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="site-logo">
+                <NavLink className="site-logo__link" to="/">Snaps</NavLink>
+            </h1>
+            <button className="filter">
+                <label className="filter__text">Filters</label>
+                <img className="filter__icon" src={ FilterSvg } alt="Filter icon."></img>
+            </button>
+        </header>
+    );
+}
+
+export default Header;
