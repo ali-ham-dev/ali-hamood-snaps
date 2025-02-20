9a07ea1cca2db1919cbd0d84952af5aebc4f8714
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-19 17:32:58 -0800

    Added gallary component as it will be the ImageCard's parent element. Adds ImageCard component. Currently place holder code has been added and App.jsx will beed to be updated later.

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import './Gallery.css';
+import ImageCard from '../ImageCard/ImageCard.jsx';
+
+function Gallery() {
+    return (
+        <section className="gallery">
+            <ImageCard />
+        </section>
+    );
+}
+
+export default Gallery;
---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,12 @@
+import './ImageCard.css';
+
+function ImageCard() {  
+    return (
+        <div className="image-card">
+            <img src="https://via.placeholder.com/150" alt="placeholder" />
+            <p>Image Title</p>
+        </div>
+    );
+}
+
+export default ImageCard;
