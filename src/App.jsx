98b67007001a84e47a0c968c3348746bff26805b
Ali <36491751+ali-ham-dev@users.noreply.github.com>
2025-02-20 18:22:29 -0800

    Revert "Snaps v01.00.0001.04 image card (#2): Card for displaying an image, i…"
    This reverts commit 1a8dfd1a774d9792cd464f6cacc6d44ac3acabcd.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Hero />
-        <Gallery />
-        <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          {/* <Route path='*' element={<Home />} /> */}
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+
     </>
-  );
+  )
 }
 
 export default App
