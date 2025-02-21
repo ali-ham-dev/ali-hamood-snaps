eaad3c751b3af1d4fa68a28561f1cd1fea2eea01
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-20 18:19:25 -0800

    Cleaned up code and hardcoded the first card.

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,20 +1,21 @@
 import './ImageCard.css';
 import Tag from '../tag/Tag.jsx';
-import tags from '../../data/tags.json';
-import photos from '../../data/photos.json';
 
-function ImageCard() {  
-    // tags.forEach(tag => { console.log(tag); });
+function ImageCard(props) {  
+    const imageSrc = props.imageSrc ?? '';
+    const imageAltText = props.imageAltText ?? '';
+    const author = props.author ?? 'Unknown';
+    const tags = props.tags ?? [];
+    console.log('Got called once');
 
     return (
         <div className="image-card">
             <div className="image-card__image-container">
-                <img src="" alt="" className="image-card__image" />
-                <div className="image-card__image-author"></div>
+                <img src={imageSrc} alt={imageAltText} className="image-card__image" />
+                <span className="image-card__image-author">{author}</span>
             </div>
             <div className="image-card__tags">
-                <Tag text={'Canada'} marginRight={true}/>
-                <Tag text={'BC'} clickable={true} />
+                { tags.map((tag, index) =>  <Tag text={tag} marginRight={(index === tags.length - 1) ? true : false}/>) }
             </div>
         </div>
     );
