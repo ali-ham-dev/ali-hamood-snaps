e367709179421f23dba0c7be4fd0f495f34d093f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-22 11:48:13 -0800

    Clickable tags clicks are now presistent.

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,12 +1,26 @@
 import './Tag.css';
+import {useState} from 'react';
 
 function Tag(props) {
     const clickableClass = props.clickable ? 'tag--clickable' : '';
     const marginRight = props.marginRight ? 'tag--margin-right' : '';
     const marginBottom = props.marginBottom ? 'tag--margin-bottom' : '';
 
+    const [clicked, setClicked] = useState(false);
+    const handelTagClick = ()=>{
+        if (!clickableClass)
+            return;
+
+        setClicked(!clicked);
+    }
+
     return (
-        <button className={`tag ${ clickableClass } ${ marginRight } ${ marginBottom }`}>{ props.text }</button>
+        <button className={`tag 
+            ${ clickableClass } 
+            ${ marginRight } 
+            ${ marginBottom } 
+            ${clicked ? 'tag--clickable-clicked' : ''}`}
+            onClick={handelTagClick}>{ props.text }</button>
     );
 }
 
