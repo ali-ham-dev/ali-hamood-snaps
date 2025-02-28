ad5c4ad0fae82c25683408153e2d15952a5719e2
Ali Hamood <ali.hamood@gov.bc.ca>
2025-02-27 18:05:04 -0800

    Adds image page for new rout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
 import React from 'react';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import ImagePage from './pages/ImagePage/ImagePage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const [filterDisplayed, setFilterDisplayed] = useState(false);
@@ -18,7 +19,7 @@ function App() {
         <Header filterDisplayed={filterDisplayed} updateFilterDisplayed={updateFilterDisplayed}/>
         <Routes>
           <Route path='/' element={ <Home filterDisplayed={filterDisplayed}/> } />
-          <Route path='/imagePage/:imageId' element={ <div>Image Page</div> } />
+          <Route path='/imagePage/:imageId' element={ <ImagePage /> } />
           <Route path='*'element={ <div>404</div> } />
         </Routes>
         <Footer />
---
+++ src/pages/ImagePage/ImagePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ImagePage.scss';
+
+function ImagePage() {
+    return (
+        <main className="image-page">
+            <div className="">
+                <h1>Image Page</h1>
+            </div>
+        </main>
+    );
+}
+
+export default ImagePage;
