cc6d0469b30aa08414c5e6119ed114ddf42c473f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-05 10:46:50 -0800

    Adds return home button

---
+++ src/components/ReturnHome/ReturnHome.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ReturnHome.scss';
+import Arrow from '../../assets/icons/Arrow.svg';
+import { useNavigate} from 'react-router-dom';
+
+function ReturnHome({onHomePage}) {
+    const navigate = useNavigate();
+
+    const handelReturnHomeClick = (e) => {
+        e.preventDefault();
+        navigate('/');
+    }
+
+    return (
+        <button className={`return-home ${onHomePage ? 'return-home--hide' : ''}`}
+            onClick={handelReturnHomeClick}>
+            <img className="return-home__icon" src={Arrow} alt="Return to home page icon."></img>
+            <label className="return-home__text">Home</label>
+        </button>
+    )
+}
+
+export default ReturnHome;
