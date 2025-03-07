610572e50909b5f2dad6330ebf80614a964197bd
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-07 11:26:28 -0800

    Adds .scss file for new componenets and updated name conflict in largeImageCard.scss file.

--- src/components/LargeImageCard/LargeImageCard.jsx
+++ src/components/LargeImageCard/LargeImageCard.jsx
@@ -10,26 +10,26 @@ function ImageCard(props) {
     const author = props.author ?? 'Unknown';
     const tags = props.tags ?? [];
     const imageId = props.imageId ?? '';
-    const filterDisplayed = props.filterDisplayed ? 'image-card--filter-displayed' : '';
+    const filterDisplayed = props.filterDisplayed ? 'large-image-card--filter-displayed' : '';
 
     // console.log(imageSrc);
 
     return (
-        <div className={`image-card ${ filterDisplayed }`}>
-            <div className="image-card__image-container">
-                <img src={imageSrc} alt={imageAltText} className="image-card__image" />
+        <div className={`large-image-card ${ filterDisplayed }`}>
+            <div className="large-image-card__image-container">
+                <img src={imageSrc} alt={imageAltText} className="large-image-card__image" />
             </div>
-            <div className="image-card__tags">
+            <div className="large-image-card__tags">
                 { tags.map((tag, index) =>  
                     <Tag text={tag} marginRight={(index === tags.length - 1) ? false : true} key={ uuidV4() } />) }
             </div>
-            <div className="image-card__image-data">
-                <div className="image-card__likes">
-                    <button className="image-card__like-button"></button>
-                    <span className="image-card__like-count">0</span>
+            <div className="large-image-card__image-data">
+                <div className="large-image-card__likes">
+                    <button className="large-image-card__like-button"></button>
+                    <span className="large-image-card__like-count">0</span>
                 </div>
-                <span className="image-card__author"></span>
-                <span className="image-card__date"></span>
+                <span className="large-image-card__author"></span>
+                <span className="large-image-card__date"></span>
             </div>
         </div>
     );
