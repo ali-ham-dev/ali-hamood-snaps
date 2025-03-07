43730afa855f6cce4d56a1a543133b2b9d787b01
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-07 13:16:09 -0800

    Adds image data and splits image data from LargeImageCard

---
+++ src/components/ImageData/ImageData.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './ImageData.scss';
+import { useState, useEffect } from 'react';
+import apiData from '../../data/apiData.json';
+import LikeIcon from '../../assets/icons/Like_Outline.svg';
+import axios from 'axios';
+
+function ImageData({ image }) {
+    const likes = image.likes ?? 0;
+    const photographer = image.photographer ?? 'Unknown';
+    const timestamp = image.timestamp ?? '';
+    const date = new Date(timestamp).toLocaleDateString('en', 
+        { month: '2-digit', day: '2-digit', year: 'numeric' });
+
+    return (
+        <div className="image-data">
+            <div className="image-data__desktop-display">
+                <div className="image-data__likes">
+                    <button className="image-data__like-button" 
+                        type="button">
+                        <img className="image-data__like-icon" src={LikeIcon} alt="Like icon"></img>
+                    </button>
+                    <span className="image-data__like-count">{`${likes} likes`}</span>
+                </div>
+                <span className="image-data__author-desktop-tablet">{`Photo by ${photographer}`}</span>
+                <span className="image-data__date">{date}</span>
+            </div>
+            <span className="image-data__author">{`Photo by ${photographer}`}</span>
+        </div>
+    )
+}
+
+export default ImageData;
--- src/components/LargeImageCard/LargeImageCard.jsx
+++ src/components/LargeImageCard/LargeImageCard.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import './LargeImageCard.scss';
 import Tag from '../Tag/Tag.jsx';
 import { v4 as uuidV4 }from 'uuid';
-import LikeIcon from '../../assets/icons/Like_Outline.svg';
+import ImageData from '../ImageData/ImageData.jsx';
 
 
 function ImageCard(props) {  
     const image = props.image ?? {};
     const imageSrc = image.photo ?? null;
     const imageAltText = image.imageAltText ?? '';
-    const likes = image.likes ?? 0;
-    const photographer = image.photographer ?? 'Unknown';
     const tags = image.tags ?? [];
-    const timestamp = image.timestamp ?? '';
-
-    console.log(image);
-
-    const date = new Date(timestamp).toLocaleDateString('en', 
-        { month: '2-digit', day: '2-digit', year: 'numeric' });
 
     return (
         <div className={"large-image-card"}>
@@ -28,19 +20,7 @@ function ImageCard(props) {
                 { tags.map((tag, index) =>  
                     <Tag text={tag} marginRight={(index === tags.length - 1) ? false : true} key={ uuidV4() } />) }
             </div>
-            <div className="large-image-card__image-data">
-                <div className="large-image-card__desktop-display">
-                    <div className="large-image-card__likes">
-                        <button className="large-image-card__like-button">
-                            <img className="large-image-card__like-icon" src={LikeIcon} alt="Like icon"></img>
-                        </button>
-                        <span className="large-image-card__like-count">{`${likes} likes`}</span>
-                    </div>
-                    <span className="large-image-card__author-desktop-tablet">{`Photo by ${photographer}`}</span>
-                    <span className="large-image-card__date">{date}</span>
-                </div>
-                <span className="large-image-card__author">{`Photo by ${photographer}`}</span>
-            </div>
+            <ImageData image={image} />
         </div>
     );
 }
