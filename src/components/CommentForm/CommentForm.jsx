df854015474ae65f0b5eb6b77c64f79fb20f0f66
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-08 12:41:39 -0800

    Comment form now accepts and validates user input.

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import './CommentForm.scss';
+import { useState, useRef } from 'react';
 
 function CommentForm() {
+    const [nameIsValid, setNameIsValid] = useState(true);
+    const [commentIsValid, setCommentIsValid] = useState(true);
+    const formRef = useRef();
+
+    const handelSubmit = (e) => {
+        e.preventDefault();
+
+        const name = formRef.current.name.value;
+        const comment = formRef.current.comment.value;
+
+        if (name === '') {
+            setNameIsValid(false);
+            alert('Please enter your name.');
+        } else {
+            setNameIsValid(true);
+        }
+
+        if (comment === '') {
+            setCommentIsValid(false);
+            alert('Please enter your comment.');
+        } else {
+            setCommentIsValid(true);
+        }
+
+        if (comment === '' || name === '') {
+            return;
+        }
+
+        console.log(name);
+        console.log(comment);
+    }
+
     return (
-        <form className="comment-form">
+        <form className="comment-form" ref={formRef}>
             <label className="comment-form__label" htmlFor="name">Name</label>
-            <input type="text" placeholder="Add your name..." className="comment-form__input-name" name="name"/>
-            <label className="comment-form__label" htmlFor="comment">Comment</label>
-            <input type="text" placeholder="Add your comment..." className="comment-form__input-comment" name="userName"/>
-            <button type="submit" className="comment-form__button">Submit</button>
+            <input type="text" 
+                className={`comment-form__input-name 
+                    ${nameIsValid ? '' : 'comment-form__input--error'}`} 
+                name="name"/>
+            <label className="comment-form__label comment-form__label--margin-top" htmlFor="comment">Comment</label>
+            <textarea type="text"  
+                className={`comment-form__input-comment
+                    ${commentIsValid ? '' : 'comment-form__input--error'}`}
+                name="comment"
+                id="comment"/>
+            <button type="button" 
+                className="comment-form__button"
+                onClick={handelSubmit}>Submit</button>
         </form>
     )
 }
